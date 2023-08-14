896544dc45edd22fab850a9aa4752259dacc22b5
MIGUEL ANGELO SILVA <1091392313002@fatec-srv.local>
2023-08-14 20:35:01 -0300

    aula 14/08

---
+++ Aula_01/exercicios3.ts
@@ -0,0 +1,17 @@
+
+
+namespace exercicios3 {
+    let nota1: number, nota2: number, nota3: number;
+    const peso1: number = 3, peso2: number = 5, peso3: number = 2;
+
+    nota1 = 8, nota2 = 3, nota3 = 8;
+
+    let situacao: boolean = nota3 == 0 ? false : true;
+
+    if(situacao){
+        let media: number = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3)/(peso1 + peso2 + peso3)
+        console.log(`A média final é: ${media}`);
+    } else {
+        console.log(`Aluno não entregou o trabalho e sua média final foi 0`);
+    }
+}
