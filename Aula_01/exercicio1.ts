82f5ba4a15ad94053dcba1d57038fcfe7c306c7e
MIGUEL ANGELO SILVA <1091392313002@fatec-srv.local>
2023-08-25 20:57:48 -0300

    (25/08)

---
+++ Aula_01/exercicio1.ts
@@ -0,0 +1,12 @@
+namespace exercicio1 {
+    let i: number;
+    i = 0
+    while(i < 53){
+        i++
+        if(i / i == 1){
+            if(i % 1 == 0){
+                console.log(`Número primo: ${i}`)
+            }
+        }
+    }
+}
